40802108d1d2300807fd28c6c7240f2074563ccf
Tom <tomyitav@gmail.com>
2016-11-28 00:34:16 +0200

    Removing Train on functionality

--- app/trains/trains.js
+++ app/trains/trains.js
@@ -10,11 +10,21 @@ angular.module('myContacts.trains', ['ngRoute'])
 }])
 
 .controller('trainCtrl', ['$scope', '$http',function($scope, $http) {
+  $scope.train= {};
   $scope.fetchTrainsList = function() {
     $http.get("http://localhost:8080/trains/trainslist.json").success(function(trainList){
       console.log(trainList)
       $scope.trains = trainList;
     });
+  };
+
+  $scope.removeTrain = function(id) {
+    console.log("logging before delete train...")
+    $http.delete("http://localhost:8080/trains/removeTrain/" + id).success(function(res) {
+      console.log(res);
+      $scope.fetchTrainsList();
+    });
+    $scope.train.name = '';
   };
 
   $scope.fetchTrainsList();
